96f3452875fea7c8b425019f8f272a15af6a613d
loyx <isluoyixuan1@163.com>
2021-04-23 14:29:50 +0800

    check sensor.py

--- sensor.py
+++ sensor.py
@@ -2,24 +2,28 @@
 class Sensor:
     SC = set()
 
-    def __init__(self, category, accuracy, s_range):
+    def __init__(self, sid, category, accuracy, s_range):
+        if sid in Sensor.SC:
+            raise ValueError(f"sensor {sid} already existed")
+        self.id = sid
+        Sensor.SC.add(sid)
         self.category = category
-        if category not in self.SC:
-            self.SC.add(category)
-
         self.accuracy = accuracy
         self.range = s_range
 
     def __repr__(self):
-        return "Sensor({}, {}, {})".format(self.category, self.accuracy, self.range)
+        return "Sensor({}: {}, {}, {})".format(self.id, self.category, self.accuracy, self.range)
+
+    def __eq__(self, other):
+        return
 
 
 def SensorGenerator():
     sensors = [
-        Sensor("摄像头", 1200, 100),
-        Sensor("可燃气体传感器", 0, 0),
-        Sensor("硫化气体传感器", 0, 0),
-        Sensor("PM2.5传感器", 0, 0)
+        Sensor(1, "摄像头", 1200, 100),
+        Sensor(2, "可燃气体传感器", 0, 0),
+        Sensor(3, "硫化气体传感器", 0, 0),
+        Sensor(4, "PM2.5传感器", 0, 0)
     ]
     return sensors
 
