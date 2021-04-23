bacc90f4cc72349e7165a87e9a6d990fdc0007ee
loyx <isluoyixuan1@163.com>
2021-04-23 16:45:06 +0800

    check robot.py

--- robot.py
+++ robot.py
@@ -57,21 +57,17 @@ class RobotCategory(ABC):
     @abstractmethod
     def getLocation(self, reg1, reg2, percentage, tr: List[Region]) -> Region:
         """
-        :param tr:
-        :param reg1:
-        :param reg2:
-        :param percentage:
-        :return:
+        该函数是robot在movingState时，根据移动距离的百分比返回所在区域
         """
 
 
 class Robot:
 
-    def __init__(self, rid, r_category, init_reg, init_loc):
+    def __init__(self, rid, r_category, init_reg):
         # static info
         self.id = rid
         self.C: RobotCategory = r_category
-        self.init_reg = init_reg
+        self.init_reg: Region = init_reg
 
         # state
         self.idleState = IdleState(self)
@@ -81,16 +77,23 @@ class Robot:
         self.state = self.idleState
 
         # dynamic location info
-        self.location: Point = init_loc
+        self.location: Point = self.init_reg.randomLoc()
         self.current_region: Region = init_reg
 
         # dynamic task info
         self.current_task_region: Optional[Region] = None
         self.current_cursor = 0
+        """
+            current_cursor指示当前执行的任务
+                           current_cursor
+                                 V
+            [finished_reg, ..., reg, ..., unfinished_reg]
+        """
 
+        # 因为current_cursor初始值为0，因此以下都需要一个占位
         # planed task info
-        self.task_in_reg: List[List] = []
-        self.sensor_in_reg: List[List[Region]] = []
+        self.task_in_reg: List[List] = [[None]]
+        self.sensor_in_reg: List[List] = [[None]]
         self.planned_path: List[Region] = [init_reg]
         # 约定；当submit后，更新为real_finish_time
         self.finish_time: List[float] = [0]
@@ -163,10 +166,6 @@ class Robot:
     def distBetweenRobot(self, r: 'Robot'):
         return EuclideanDistance(self.location, r.location)
 
-    def arrivalTime(self, reg: Region):
-        move_time = self.C.interD(self.planned_path[-1], reg) / self.C.v
-        return self.finish_time[-1] + move_time
-
     def possiblePlan(self, reg, task):
         """
         机器人在reg区域执行task的可能_时间点_和使用的传感器
@@ -185,13 +184,12 @@ class Robot:
         # 因为机器人执行任务的流程一定是从上一区域移动到此区域，之后再完成任务
         move_time = self.C.interD(self.planned_path[-1], reg) / self.C.v
 
-        # 当机器人为sensingState时，不能并发分配任务
-        if self.state == self.sensingState:
+        # 当机器人为sensingState时，或位于初始位置，不能并发分配任务
+        if self.state == self.sensingState or self.current_cursor == 0:
             return self.finish_time[-1] + self.C.intraD(reg) / self.C.v + move_time
 
-        check_init = self.planned_path[-1] == self.init_reg
-        if not check_init and move_time == 0 and sensor not in self.sensor_in_reg[reg]:
-            # 如果机器人目的区域仍是机器人之前的区域，且之前没有使用该传感器，且不是初始状态
+        if move_time == 0 and sensor not in self.sensor_in_reg[reg]:
+            # 如果机器人目的区域仍是机器人之前的区域，且之前没有使用该传感器
             # 则可以并行执行，理想完成时间点为之前的完成时间
             return self.finish_time[-1]
         return self.finish_time[-1] + self.C.intraD(reg) / self.C.v + move_time
@@ -199,6 +197,6 @@ class Robot:
     def moveDistance(self):
         dis = 0
         pre_reg = self.init_reg
-        for reg in self.planned_path[2:]:
+        for reg in self.planned_path[1:]:
             dis += self.C.intraD(reg) + self.C.interD(pre_reg, reg)
         return dis
