75256a3bb0dd00cdedd200ad4306b749fbba9051
loyx <isluoyixuan1@163.com>
2021-04-24 10:53:52 +0800

    Sensor 添加单位str

--- sensor.py
+++ sensor.py
@@ -2,18 +2,20 @@
 class Sensor:
     SC = set()
 
-    def __init__(self, sid, category, accuracy, s_range):
+    def __init__(self, sid, category, accuracy, a_unit, s_range, r_unit):
         if sid in Sensor.SC:
             raise ValueError(f"sensor {sid} already existed")
         self.id = sid
         Sensor.SC.add(sid)
         self.category = category
         self.accuracy = accuracy
+        self.a_unit = a_unit
         self.range = s_range
+        self.r_unit = r_unit
 
     def __repr__(self):
-        return "Sensor(id:{}: category:{}, acc:{}, range:{})"\
-            .format(self.id, self.category, self.accuracy, self.range)
+        return "Sensor(id:{}: category:{}, acc:{}{}, range:{}{})"\
+            .format(self.id, self.category, self.accuracy, self.a_unit, self.range, self.r_unit)
 
     def __eq__(self, other):
         return
